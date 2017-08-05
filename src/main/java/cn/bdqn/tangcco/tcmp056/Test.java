760d4cd2548e0793f5ad02e160b1abc83d2dde55
houjianhao <934163168@qq.com>
2017-08-05 15:16:04 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/Test.java
@@ -0,0 +1,11 @@
+package cn.bdqn.tangcco.tcmp056;
+
+/**
+ * Created by MACHENIKE on 2017/8/5.
+ */
+public class Test {
+    @org.junit.Test
+    public static void main(String[] args) {
+        System.out.println("一直努力");
+    }
+}
